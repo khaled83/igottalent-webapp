8e9907676823c476785237a71d4dce8c82588a24
khaled83 <khaled@supahands.com>
2018-06-02 23:35:48 +0800

    Enhanced login page layout

--- app/adapters/video.js
+++ app/adapters/video.js
@@ -1,5 +1,14 @@
 import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
-  host: 'http://localhost:3000'
+  host: 'http://localhost:3000',
+  
+  urlForQuery (query) {
+    if (query.filter) {
+      delete query.filter;
+      return this._super(...arguments) + '/unauthorized';
+    }
+    
+    return this._super(...arguments);
+  }
 });
--- app/routes/login.js
+++ app/routes/login.js
@@ -2,4 +2,11 @@ import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-
 import Route from '@ember/routing/route';
 
 export default Route.extend(UnauthenticatedRouteMixin, {
+  model() {
+    return this.store.query('video', {
+      filter: {
+        unauthorized: true
+      }
+    });
+  }
 });
