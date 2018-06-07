8c4f916427d217f89fb99a1458e7fd33063901df
khaled83 <khaled@supahands.com>
2018-06-07 15:04:43 +0800

    New/edit video forms enhanced

--- app/models/video.js
+++ app/models/video.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import EmberObject, { computed } from '@ember/object';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
@@ -8,10 +9,24 @@ export default DS.Model.extend({
   approvedAdmin: DS.attr('boolean'),
   approvedUser: DS.attr('boolean'),
   live: DS.attr('boolean'),
-  
+
   // ASSOCIATIONS //
-  user: DS.belongsTo('user')
-  
+  user: DS.belongsTo('user'),
+
   //=== COMPUTED PROPERTIES ===
-  
+  isSaved: computed.not('isNew'),
+  videoIdComputed: computed('url', function() {
+    let videoId = this.get('videoId');
+    if (videoId) {
+      return videoId;
+    }
+
+    let url = this.get('url');
+    if (!url) {
+      return '';
+    }
+
+    let regExp = /(youtu.be\/|youtube.com\/(watch\?(.*&)?v=|(embed|v)\/))([^\?&\"\'>]+)/;
+    return url.match(regExp)[5];
+  }),
 });
