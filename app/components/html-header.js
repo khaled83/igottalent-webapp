44c4523eaa14dc6cb0bc3d9bdbfbe2bbf2c7b20f
khaled83 <khaled@supahands.com>
2018-06-13 13:53:41 +0800

    Error state avatar placeholder

--- app/components/html-header.js
+++ app/components/html-header.js
@@ -4,6 +4,14 @@ import { inject as service } from '@ember/service';
 export default Component.extend({
   currentUser: service(),
   
+  didInsertElement: function() {
+    // TODO: move this to html-img
+    // Replace avatar when error detected
+    $('img.avatar').on('error', function(){
+      $(this).attr('src', 'img/avatar.png');
+    });
+  },
+  
   actions: {
     logout() {
       this.get('logout')();
