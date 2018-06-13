f263ee72962de79fc286e7a0891448a2e91cb1e2
khaled83 <khaled@supahands.com>
2018-06-13 13:54:56 +0800

    Facebook social data-href dynamically set from current page url

--- app/components/facebook-social-comments.js
+++ app/components/facebook-social-comments.js
@@ -1,4 +1,8 @@
 import Component from '@ember/component';
+import EmberObject, { computed } from '@ember/object';
 
 export default Component.extend({
+  dataHref: function() {
+    return window.location.href;
+  }.property('currentPath')
 });
