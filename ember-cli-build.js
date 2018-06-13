16ab356298f2b6314afa729fb217abf46a4cf32d
khaled83 <khaled@supahands.com>
2018-06-13 09:53:28 +0800

    Install ember-cli-sass, jquery optional features and integration

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,11 +4,17 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    'ember-bootstrap': {
-      'bootstrapVersion': 4,
-      'importBootstrapFont': false,
-      'importBootstrapCSS': true
+    // minifyCSS: {
+    //   options: { processImport: true }
+    // },
+    sassOptions: {
+      extension: 'sass'
     }
+    // 'ember-bootstrap': {
+    //   'bootstrapVersion': 4,
+    //   'importBootstrapFont': false,
+    //   'importBootstrapCSS': true
+    // },
   });
 
   // Use `app.import` to add additional libraries to the generated
