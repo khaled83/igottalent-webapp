037d92c20488fd1cab43748dad5f476c6d4683e3
khaled83 <khaled@supahands.com>
2018-06-26 17:02:12 +0800

    Toggle admin user

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,12 +4,18 @@ import { inject as service } from '@ember/service';
 
 export default Controller.extend({
   session: service(),
+  store: service(),
   currentUser: service('current-user'),
-  
+
   config: config.torii.providers['facebook-oauth2'],
   actions: {
     logout() {
       this.get('session').invalidate();
+    },
+    toggleAdmin() {
+      let user = this.get('currentUser.user');
+      user.toggleProperty('admin');
+      user.save();
     }
   }
 });
